f2e2ea6d7d5f4205557e643e829e3332647ef5e7
raztem <raznoetema@gmail.com>
2024-10-05 17:36:37 +0300

    initial commit

--- apiData.js
+++ apiData.js
@@ -1,7 +1,6 @@
 import fetch from "node-fetch";
 
-const url =
-  "https://newsdata.io/api/1/news?country=ua&size=10&language=uk&apikey=pub_39547913a0efdae4ded7dc3f9341b00f6a608";
+const url = "";
 
 let articles = [];
 
--- mongoDb.js
+++ mongoDb.js
@@ -2,32 +2,32 @@ import { MongoClient } from "mongodb";
 import { fetchArticles } from "./apiData.js";
 
 const uri =
-  "mongodb+srv://raztem:raztem@cluster0.qwkwkxo.mongodb.net?retryWrites=true&w=majority";
+  // "mongodb+srv://raztem:raztem@cluster0.qwkwkxo.mongodb.net?retryWrites=true&w=majority";
 
-async function run() {
-  const client = new MongoClient(uri);
+  async function run() {
+    const client = new MongoClient(uri);
 
-  try {
-    await client.connect();
-    const dbName = "NewsDb";
-    const collectionName = "data";
-    const database = client.db(dbName);
-    const collection = database.collection(collectionName);
+    try {
+      await client.connect();
+      const dbName = "NewsDb";
+      const collectionName = "data";
+      const database = client.db(dbName);
+      const collection = database.collection(collectionName);
 
-    // Очищуємо колекцію перед вставкою нових даних для уникнення дублікатів
-    await collection.deleteMany({});
+      // Очищуємо колекцію перед вставкою нових даних для уникнення дублікатів
+      await collection.deleteMany({});
 
-    const data = await fetchArticles();
-    const insertManyResult = await collection.insertMany(data);
-    console.log(
-      `${insertManyResult.insertedCount} documents successfully inserted.\n`
-    );
-  } catch (err) {
-    console.error(`Something went wrong: ${err}`);
-  } finally {
-    await client.close();
-  }
-}
+      const data = await fetchArticles();
+      const insertManyResult = await collection.insertMany(data);
+      console.log(
+        `${insertManyResult.insertedCount} documents successfully inserted.\n`
+      );
+    } catch (err) {
+      console.error(`Something went wrong: ${err}`);
+    } finally {
+      await client.close();
+    }
+  };
 
 // Запускаємо оновлення даних кожну годину
 // run();
